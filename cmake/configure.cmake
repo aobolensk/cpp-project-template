e36a988546597210885b6f3e411901f3336caf84
Arseniy Obolenskiy <gooddoog@student.su>
2024-10-29 23:13:01 +0800

    Add more warnings and treat them as errors (#10)

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -5,3 +5,13 @@ foreach(CONFIG "" _DEBUG _RELEASE)
 endforeach()
 
 set(CMAKE_CXX_STANDARD 20)
+
+if(UNIX)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wsign-compare -Werror")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wsign-compare -Werror")
+endif(UNIX)
+
+if(MSVC)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W4 /wd4267 /wd4244 /wd4100 /WX")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4 /wd4267 /wd4244 /wd4100 /WX")
+endif(MSVC)
