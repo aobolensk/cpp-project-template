48d9263423e9fb603dbc263e961cc31482c89267
Arseniy Obolenskiy <gooddoog@student.su>
2025-01-09 23:14:23 +0100

    Add -Wextra for Linux and macOS builds

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -7,8 +7,8 @@ endforeach()
 set(CMAKE_CXX_STANDARD 20)
 
 if(UNIX)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wsign-compare -Werror")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wsign-compare -Werror")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Wsign-compare -Werror")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wsign-compare -Werror")
 endif(UNIX)
 
 if(MSVC)
